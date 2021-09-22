724130e0c65b9a34d485a4b1b3491603dd2b8e50
Chris Penny <chris.penny@gmail.com>
2021-09-22 13:10:45 +1200

    Fix broken unit test. Expect img-src for Youtube

--- tests/PolicyTest.php
+++ tests/PolicyTest.php
@@ -271,7 +271,7 @@ class PolicyTest extends SapphireTest
         [$request, $response] = $this->getRequestResponse();
         $policy->applyTo($response);
         $this->assertEquals(
-            'script-src www.youtube.com s.ytimg.com player.vimeo.com;frame-src *.youtube.com player.vimeo.com;child-src player.vimeo.com',
+            'img-src *.ytimg.com;script-src www.youtube.com s.ytimg.com player.vimeo.com;frame-src *.youtube.com player.vimeo.com;child-src player.vimeo.com',
             $response->getHeader('content-security-policy')
         );
     }
