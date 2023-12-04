5f976cfa1827200d40c93fed59e878be799d5f8b
Ed Wilde <ed.wilde@silverstripe.com>
2023-12-05 11:34:36 +1300

    Add other rules required by Hotjar, unsafe inline for CSS/JS and web sockets
    As mentioned in the linked CSP page

--- src/Fragments/Hotjar.php
+++ src/Fragments/Hotjar.php
@@ -3,6 +3,7 @@
 namespace Silverstripe\CSP\Fragments;
 
 use Silverstripe\CSP\Directive;
+use Silverstripe\CSP\Keyword;
 use Silverstripe\CSP\Policies\Policy;
 
 /**
@@ -12,6 +13,7 @@ class Hotjar implements Fragment
 {
     public static function addTo(Policy $policy): void
     {
+        // Set domains to be allowed for various directives
         $domains = [
             '*.hotjar.com',
             '*.hotjar.io',
@@ -23,5 +25,14 @@ class Hotjar implements Fragment
             ->addDirective(Directive::CONNECT, $domains)
             ->addDirective(Directive::FRAME, $domains)
             ->addDirective(Directive::FONT, $domains);
+
+        // Allow inline scripts
+        $policy->addDirective(Directive::SCRIPT, Keyword::UNSAFE_INLINE);
+
+        // Allow inline styles
+        $policy->addDirective(Directive::STYLE, Keyword::UNSAFE_INLINE);
+
+        // Allow web sockets
+        $policy->addDirective(Directive::CONNECT, 'wss://*.hotjar.com');
     }
 }
