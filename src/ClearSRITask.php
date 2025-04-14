5c879fb09421fe1ee22bec15c197f278871a840d
Mohamed Alsharaf <mohamed.alsharaf@silverstripe.com>
2025-04-14 13:48:09 +1200

    Add support for CMS 6

--- src/ClearSRITask.php
+++ src/ClearSRITask.php
@@ -7,19 +7,20 @@ use Silverstripe\CSP\Requirements\SRIRecord;
 use SilverStripe\Dev\BuildTask;
 use SilverStripe\ORM\DataObjectSchema;
 use SilverStripe\ORM\DB;
+use SilverStripe\PolyExecution\PolyOutput;
+use Symfony\Component\Console\Input\InputInterface;
 
 class ClearSRITask extends BuildTask
 {
-    protected $title = 'Clear SRI hashes';
-    protected $description = 'This allow you to reset the tokens incase they change on the server';
+    protected string $title = 'Clear SRI hashes';
+    protected static string $description = 'This allow you to reset the tokens incase they change on the server';
 
-    /**
-     * @param HTTPRequest|mixed $request
-     */
-    public function run($request): void
+    protected function execute(InputInterface $input, PolyOutput $output): int
     {
         $tableName = DataObjectSchema::singleton()->tableName(SRIRecord::class);
         DB::get_conn()->clearTable($tableName);
         echo 'Records cleared';
+
+        return 0;
     }
 }
